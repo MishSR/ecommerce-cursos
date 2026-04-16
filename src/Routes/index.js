9af1550b33894d384de97296a490d4f08b288f5a
MishSR <analisk17@gmail.com>
2026-04-15 18:10:30 -0600

    index routes updated

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,3 +1,18 @@
 import express from 'express';
-import {body, params}   from 'express-validator';
-import {getCart, getCartById, getCartByUser} from '../Controllers/cartController.js';
+import paymentRoutes from './paymentRoutes.js';
+import userRoutes from './userRoutes.js';
+import coursesRoutes from './coursesRoutes.js';
+import cartRoutes from './cartRoutes.js';
+import wishListRoutes from './wishListRoutes.js';
+import reviewsRoutes from './reviewsRoutes.js';
+
+ const router = express.Router();
+
+ router.use('/users', userRoutes);
+ router.use('/courses', coursesRoutes);
+ router.use('/cart', cartRoutes);
+ router.use('/wishlist', wishListRoutes);
+ router.use('/payment', paymentRoutes);
+ router.use('/reviews', reviewsRoutes);
+
+ export default router;
